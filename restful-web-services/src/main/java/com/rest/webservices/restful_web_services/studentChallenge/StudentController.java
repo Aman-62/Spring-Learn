37d2858e04e686b521eec9d590929010788945cf
Aman-62 <aman230630@gmail.com>
2024-08-05 15:19:12 +0530

    Connect to h2

--- restful-web-services/src/main/java/com/rest/webservices/restful_web_services/studentChallenge/StudentController.java
+++
@@ -1,8 +0,0 @@
-package com.rest.webservices.restful_web_services.studentChallenge;
-
-public class StudentController {
-
-    // area/5/4
-
-    // "Area of rectangle with length 5 and breath 4 is 20"
-}
---
+++ restful-web-services/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO user_details(id,birth_date,name)
+VALUES (1001, current_date(), 'Vishal');
