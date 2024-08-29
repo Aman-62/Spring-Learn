cc88789a1b929defd834d0d1edf630a27eab9144
Aman-62 <aman230630@gmail.com>
2024-08-29 14:59:00 +0530

    list todos

--- todo-full-stack/src/components/TodoApp.jsx
+++ todo-full-stack/src/components/TodoApp.jsx
@@ -1,5 +1,12 @@
 import { useState } from "react";
-import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useNavigate,
+  useParams,
+  Link,
+} from "react-router-dom";
 
 function TodoApp() {
   return (
@@ -8,7 +15,10 @@ function TodoApp() {
         <Routes>
           <Route path="/" element={<LoginComponent />} />
           <Route path="/login" element={<LoginComponent />} />
-          <Route path="/welcome" element={<WelcomeComponent />} />
+          <Route path="/welcome/:username" element={<WelcomeComponent />} />
+          <Route path="/todos" element={<ListTodosComponent />} />
+
+          <Route path="*" element={<NotFoundComponent />} />
         </Routes>
       </BrowserRouter>
     </div>
@@ -38,7 +48,7 @@ function LoginComponent() {
     if (username === "admin" && password === "root1234") {
       setShowSuccessMessage(true);
       setShowErrorMessage(false);
-      navigate("/welcome");
+      navigate(`/welcome/${username}`);
     } else {
       setShowErrorMessage(true);
       setShowSuccessMessage(false);
@@ -46,6 +56,7 @@ function LoginComponent() {
   }
   return (
     <div className="login">
+      <h1>Login page</h1>
       {showSuccessMessage && <div>Authenticated Successfully</div>}
       {showErrorMessage && (
         <div>Authentication Failed, Please check your credentials</div>
@@ -80,5 +91,84 @@ function LoginComponent() {
 }
 
 function WelcomeComponent() {
-  return <div className="Welcome">WelcomeComponent</div>;
+  const params = useParams();
+  const { username } = params;
+
+  return (
+    <div className="welcome">
+      <h1>Welcome Todo App</h1>
+      <p>Welcome, {username}</p>
+      <p>
+        Check your <Link to="/todos">todos</Link>
+      </p>
+    </div>
+  );
+}
+
+function NotFoundComponent() {
+  return (
+    <div className="notFoundComponent">
+      <h1>We are working hard!</h1>
+      <p>Apologies for the 404. Reach out to our team at ABC-DEF-GHI</p>
+    </div>
+  );
+}
+
+function ListTodosComponent() {
+  const today = new Date();
+  const targetDate = new Date(
+    today.getFullYear(),
+    today.getMonth() + 3,
+    today.getDay()
+  );
+
+  const todos = [
+    {
+      id: 1,
+      description: "Learn AWS",
+      done: false,
+      targetDate: targetDate,
+    },
+    {
+      id: 2,
+      description: "Learn React",
+      done: false,
+      targetDate: targetDate,
+    },
+    {
+      id: 3,
+      description: "Learn DevOps",
+      done: false,
+      targetDate: targetDate,
+    },
+  ];
+  return (
+    <div className="listTodosComponent">
+      <h1>Things you want to do!</h1>
+      <div>
+        <table border={2}>
+          <thead>
+            <tr>
+              <th>id</th>
+              <th>description</th>
+              <th>is Done?</th>
+              <th>target date</th>
+            </tr>
+          </thead>
+          <tbody>
+            {todos.map((todo) => {
+              return (
+                <tr key={todo.id}>
+                  <td>{todo.id}</td>
+                  <td>{todo.description}</td>
+                  <td>{todo.done.toString()}</td>
+                  <td>{todo.targetDate.toString()}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
